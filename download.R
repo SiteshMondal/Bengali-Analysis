675f81c14fbdc82ff43aa5eaeada457ab95190d3
smashingpumpkin <smk.chtrg@gmail.com>
2016-09-18 00:55:02 +0530

    Modified the download script

--- download.R
+++ download.R
@@ -2,6 +2,11 @@ library(twitteR)
 
 # First create a CSV file with twitter app credentials with header
 # API_KEY,API_SECRET,ACCESS_TOKEN,ACCESS_TOKEN_SECRET
-secrets <- read.csv(file = 'credentials.csv', header = TRUE, sep = ',')
 
-setup_twitter_oauth(secrets$API_KEY, secrets$API_SECRET, access_token = secrets$ACCESS_TOKEN, access_secret = secrets$ACCESS_TOKEN_SECRET)
+# Load secrets, keywords and geolocations
+secrets <- read.csv(file = 'credentials.csv', header = TRUE, sep = ',')
+keywords <- read.csv(file = 'keywords.csv')
+locations <- read.csv(file = 'geolocations.csv')
+
+# Set-up twitter oauth authentication
+setup_twitter_oauth(secrets$KEY, secrets$SECRET, access_token = secrets$TOKEN, access_secret = secrets$TOKEN_SECRET)
